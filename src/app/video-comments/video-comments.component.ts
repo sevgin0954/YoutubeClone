fc7cd367adc42b22bdc2bffd7a577906d6aeb517
sevgin0954 <skateboardista_0954@abv.bg>
2020-01-22 23:54:17 +0200

    Fixed bugs in video comment component

--- src/app/video-comments/video-comments.component.ts
+++ src/app/video-comments/video-comments.component.ts
@@ -36,6 +36,8 @@ export class VideoCommentsComponent implements OnChanges {
     this.nextPageToken = undefined;
     this.isFirstPage = true;
     this.commentThreads = [];
+
+    this.loadComments();
   }
 
   @HostListener("window:scroll")
