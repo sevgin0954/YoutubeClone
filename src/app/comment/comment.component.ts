5b26edacacdb3399b66b79a2894a4f50d49b09b7
sevgin0954 <skateboardista_0954@abv.bg>
2020-01-23 20:38:31 +0200

    Refactored code

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Comment } from '../models/comment/comment';
-import { FormatterService } from '../services-singleton/formatter.service';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
-export class CommentComponent implements OnInit {
+export class CommentComponent {
 
   @Input() comment: Comment;
   maxDisplayedCharacters: number = 100;
-
-  constructor (
-    private formatterService: FormatterService
-  ) { }
-
-  ngOnInit() {
-
-  }
 
   getAuthorChannelId(): string {
     const authorChannelIdObject = this.comment.snippet.authorChannelId;
