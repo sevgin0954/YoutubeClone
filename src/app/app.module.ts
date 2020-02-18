532d9b399df137d1dc6e4312506d766d3c1a9822
sevgin0954 <skateboardista_0954@abv.bg>
2020-02-18 14:38:26 +0200

    Refactored code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentModule } from './comment/comment.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { JwtInterceptorService } from './services-singleton/jwt-interceptor.service';
-import { SigninComponent } from './signin/signin.component';
 import { MostPopularComponent } from './most-popular/most-popular.component';
-import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
-import { SharedModule } from './shared/shared.module';
-import { NavbarTopComponent } from './navbar-top/navbar-top.component';
 import { NavbarSideComponent } from './navbar-side/navbar-side.component';
-import { CommentModule } from './comment/comment.module';
+import { NavbarTopComponent } from './navbar-top/navbar-top.component';
+import { SharedModule } from './shared/shared.module';
+import { SigninComponent } from './signin/signin.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
 import { VideoMiniModule } from './video-mini/video-mini.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    MostPopularComponent,
     SubscriptionsComponent,
+    MostPopularComponent,
     NavbarSideComponent,
-    NavbarTopComponent
+    NavbarTopComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    CommentModule,
     HttpClientModule,
     SharedModule,
-    CommentModule,
-    VideoMiniModule
+    VideoMiniModule,
   ],
   providers: [
     {
