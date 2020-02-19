5b289636963daa82eae3220d90826e8af8776c6f
sevgin0954 <skateboardista_0954@abv.bg>
2020-02-19 19:58:50 +0200

    Refactored channel-section.component

--- src/app/channel/channel-sections/channel-sections.component.ts
+++ src/app/channel/channel-sections/channel-sections.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { ChannelSectionService } from 'src/app/services-singleton/channel-section.service';
 import { ChannelSection } from 'src/app/models/channel-section/channel-section';
-import { SnippetType } from 'src/app/shared/enums/snippet-type';
 import { ChannelSectionStyle } from 'src/app/shared/enums/channel-section-style';
+import { SnippetType } from 'src/app/shared/enums/snippet-type';
 
 @Component({
   selector: 'app-channel-sections',
@@ -13,32 +13,22 @@ import { ChannelSectionStyle } from 'src/app/shared/enums/channel-section-style'
 export class ChannelSectionsComponent implements OnInit {
 
   @Input() channelId: string;
-  multipleChannelsSections: ChannelSection[];
-  singlePlaylistSections: ChannelSection[];
+  channelSections: ChannelSection[];
   snippetStyle: typeof ChannelSectionStyle = ChannelSectionStyle;
+  snippetType: typeof SnippetType = SnippetType;
 
   constructor(
     private channelSectionService: ChannelSectionService
-  ) {
-    this.multipleChannelsSections = [];
-    this.singlePlaylistSections = [];
+  ) { }
+
+  getSectionType(channelSection: ChannelSection): string {
+    return channelSection.snippet.type;
   }
 
   ngOnInit() {
     this.channelSectionService.getByChannelId(this.channelId).subscribe(channels => {
-      this.initChannels(channels);
-    });
-  }
-
-  initChannels(channels: ChannelSection[]): void {
-    channels.forEach(channel => {
-      const channelType = channel.snippet.type;
-      if (SnippetType[SnippetType.multipleChannels] === channelType) {
-        this.multipleChannelsSections.push(channel);
-      }
-      else if (SnippetType[SnippetType.singlePlaylist] === channelType) {
-        this.singlePlaylistSections.push(channel);
-      }
+      const sortedChannels = channels.sort((a, b) => a.snippet.position - b.snippet.position);
+      this.channelSections = sortedChannels;
     });
   }
 }
