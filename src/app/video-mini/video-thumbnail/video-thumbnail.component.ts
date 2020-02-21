dc1ed02e5640d2bc720039dce8b6f0c5928db1ea
sevgin0954 <skateboardista_0954@abv.bg>
2020-02-21 19:37:00 +0200

    Fixed bug when trying to access missing thumbnail

--- src/app/video-mini/video-thumbnail/video-thumbnail.component.ts
+++ src/app/video-mini/video-thumbnail/video-thumbnail.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 
 import { Video } from 'src/app/models/video/video';
 import { VideoThumbnailSize } from 'src/app/shared/enums/video-thumbnail-size';
+import { ThumbnailsService } from 'src/app/services-singleton/thumbnails.service';
+import { VideoThumbnails } from 'src/app/models/thumbnail/video-thumbnails';
 
 @Component({
   selector: 'app-video-thumbnail',
@@ -12,6 +14,16 @@ export class VideoThumbnailComponent {
 
   @Input() video: Video;
   @Input() private size: VideoThumbnailSize;
+
+  constructor(
+    private thumbnailsService: ThumbnailsService
+  ) { }
+
+  getThumbnailUrl(thumbnails: VideoThumbnails): string {
+    const url = this.thumbnailsService.getThumbnailUrl(this.size, thumbnails);
+
+    return url;
+  }
 
   get sizeString(): string {
     return VideoThumbnailSize[this.size];
