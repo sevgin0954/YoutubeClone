53650b02dadeb0dc7366cc758558954908fda949
sevgin0954 <skateboardista_0954@abv.bg>
2020-03-10 02:33:20 +0200

    Added tests and refactored code in window service

--- src/app/services-singleton/window.service.spec.ts
+++ src/app/services-singleton/window.service.spec.ts
@@ -1,12 +1,220 @@
-import { TestBed } from '@angular/core/testing';
-
 import { WindowService } from './window.service';
+import { ExceptionConstants } from '../shared/Constants/exception-constants';
+
+let service: WindowService;
+
+beforeEach(() => {
+  service = new WindowService();
+});
 
 describe('WindowService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: WindowService = TestBed.get(WindowService);
     expect(service).toBeTruthy();
   });
+});
+
+describe('WindowService\s isElementOverflowing method', () => {
+
+  const screenWidth = 800;
+  const screenHeight = 800;
+
+  beforeEach(() => {
+    spyOnProperty(window.screen, 'width').and.returnValue(screenWidth);
+    spyOnProperty(window.screen, 'height').and.returnValue(screenHeight);
+  });
+
+  it('with null element should throw an exception', () => {
+    // Arrange
+    const element = null;
+    const exceptionRegex = new RegExp(ExceptionConstants.NULL_OR_UNDEFINED);
+
+    // Act
+
+    // Assert
+    expect(() => service.isElementOverflowing(element)).toThrowError(exceptionRegex);
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with not overflowing element at the bottom right edge should return false', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementX = screenWidth - elementSize;
+      const elementY = screenHeight - elementSize;
+      setupElementGetBoundingClientRectStub(element, elementX, elementY, elementSize);
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeFalsy();
+    });
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with not overflowing element at the bottom left edge should return false', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementX = 0;
+      const elementY = screenHeight - elementSize;
+      setupElementGetBoundingClientRectStub(element, elementX, elementY, elementSize);
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeFalsy();
+    });
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with not overflowing element at the top left edge should return false', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementX = 0;
+      const elementY = 0;
+      setupElementGetBoundingClientRectStub(element, elementX, elementY, elementSize);
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeFalsy();
+    });
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with not overflowing element at the top right edge should return false', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementX = screenWidth - elementSize;
+      const elementY = 0;
+      setupElementGetBoundingClientRectStub(element, elementX, elementY, elementSize);
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeFalsy();
+    });
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with element overflowing from right should return true', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementStartPointX = screenWidth - elementSize;
+      const elementStartPointY = 0;
+      setupElementGetBoundingClientRectStub(
+        element,
+        elementStartPointX + 1,
+        elementStartPointY,
+        elementSize
+      );
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeTruthy();
+    });
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with element overflowing from botton should return true', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementStartPointX = 0;
+      const elementStartPointY = screenHeight - elementSize;
+      setupElementGetBoundingClientRectStub(
+        element,
+        elementStartPointX,
+        elementStartPointY + 1,
+        elementSize
+      );
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeTruthy();
+    });
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with element overflowing from left should return true', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementStartPointX = 0;
+      const elementStartPointY = 0;
+      setupElementGetBoundingClientRectStub(
+        element,
+        elementStartPointX - 1,
+        elementStartPointY,
+        elementSize
+      );
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeTruthy();
+    });
+  });
+
+  [
+    0,
+    10
+  ].forEach(elementSize => {
+    it('with element overflowing from top should return true', () => {
+      // Arrange
+      const element = document.createElement('div');
+      const elementStartPointX = 0;
+      const elementStartPointY = 0;
+      setupElementGetBoundingClientRectStub(
+        element,
+        elementStartPointX ,
+        elementStartPointY - 1,
+        elementSize
+      );
+
+      // Act
+      const result = service.isElementOverflowing(element);
+
+      // Assert
+      expect(result).toBeTruthy();
+    });
+  });
+
+  function setupElementGetBoundingClientRectStub(
+    element: Element,
+    elementXStartPosition: number,
+    elementYStartPosition: number,
+    elementSize: number
+  ): void {
+
+    const newRect = new DOMRect(elementXStartPosition, elementYStartPosition, elementSize, elementSize);
+    spyOn(element, 'getBoundingClientRect').and.returnValue(newRect);
+  }
 });
--- src/app/services-singleton/window.service.ts
+++ src/app/services-singleton/window.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
+import { DataValidator } from '../shared/Validation/data-validator';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WindowService {
 
+  // TODO: Should't work for hidden elements
   isElementOverflowing(element: Element): boolean {
+    DataValidator.nullOrUndefinied(element, 'element');
+
+    let isOverflowing = this.isElementOverflowingRecursive(element);
+
+    return isOverflowing;
+  }
+
+  private isElementOverflowingRecursive(element: Element): boolean {
     let isOverflowing = false;
 
     let rect: ClientRect = element.getBoundingClientRect();
-    if (rect.right === 0) {
+
+    const isElemntHidden = element.hasAttribute('hidden');
+    if (isElemntHidden) {
       element.removeAttribute('hidden');
 
       rect = element.getBoundingClientRect();
@@ -17,11 +29,22 @@ export class WindowService {
 
       element.setAttribute('hidden', 'hidden');
     }
-    else if (rect.right > window.screen.width) {
+    else if (this.isElementInsideTheScreen(rect) === false) {
       isOverflowing = true;
     }
 
     return isOverflowing;
+  }
+
+  private isElementInsideTheScreen(elementRect: ClientRect): boolean {
+    const windowScreen = window.screen;
+
+    const isTopSideInside = elementRect.top >= 0;
+    const isRightSideInside = elementRect.right <= windowScreen.width;
+    const isBottomSideInside = elementRect.bottom <= windowScreen.height;
+    const isLeftSideInside = elementRect.left >= 0;
+
+    return isTopSideInside && isRightSideInside && isBottomSideInside && isLeftSideInside;
   }
 
   onReachBottom(callback: Function): void {
