284ca42013b2ee9dfc4e5d9d0a5295af2b3a6378
sevgin0954 <skateboardista_0954@abv.bg>
2020-03-25 20:32:54 +0200

    Refactored category module

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { CategoriesComponent } from './categories/categories.component';
 
-
+const routes: Routes = [
+  { path: ':categories', component:  }
+];
 
 @NgModule({
-  declarations: [CategoriesComponent],
+  declarations: [
+    CategoriesComponent
+  ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
