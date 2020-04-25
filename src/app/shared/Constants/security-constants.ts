f51eb3cbdfc303a3a2c9569b9d57b48a32721c59
sevgin0954 <skateboardista_0954@abv.bg>
2020-04-25 23:05:46 +0300

    Refactored styles and replaces cliend id

--- src/app/shared/Constants/security-constants.ts
+++ src/app/shared/Constants/security-constants.ts
@@ -1,5 +1,4 @@
 export class SecurityConstants {
-  public static YOUTUBE_CLIENT_ID = '576498499876-u841pl14j9pdgemtlaqk1a6tjih8vb2c.apps.googleusercontent.com';
-  public static YOUTUBE_CLIENT_ID_2 = '480889960057-qfp4tfrgudk5vo6kgt6jf4fncn31ka2p.apps.googleusercontent.com';
+  public static YOUTUBE_CLIENT_ID = '816208399900-s4cjtp5e8saujtihs8taeof3e2e4u43q.apps.googleusercontent.com';
   public static IPSTACK_API_KEY = 'b81d23981d653be225d3a45032411940';
 }
